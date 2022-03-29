f6e4f50f8170fcca4b67d38aad8d8ff07cd2774c
StiriDisneyPlusRO <78642931+StiriDisneyPlusRO@users.noreply.github.com>
2022-03-29 14:49:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Feb 6, 2021 14:55:00").getTime();
+var countDownDate = new Date("June 14, 2022 10:00:00").getTime();
 
 // Actualizează numărătoarea inversă în fiecare secundă.
 var x = setInterval(function() {
@@ -26,6 +26,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = '<div style="text-align:center; color: white"><font style="font-family: \'Lexend Mega\', sans-serif"> Încă nu a fost anunțată o dată de lansare. </font></div>';
+    document.getElementById("countdown").innerHTML = '<div style="text-align:center; color: white"><font style="font-family: \'Lexend Mega\', sans-serif"> Disney+ este acum disponibil în România. <br> <a href="https://www.disneyplus.com/">Abonați-vă acum.</a> </font></div>';
   }
 }, 1000);
